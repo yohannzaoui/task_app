d83d73836ec2d4e89d12e480f7a40bee0a8fd1d0
Yohann Zaoui <yohannzaoui@gmail.com>
2019-03-01 17:57:20 +0100

    add users list and delete user function for admin

---
+++ src/Controller/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserController
+ *
+ * @package App\Controller
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user_index", methods={"GET"})
+     *
+     * @param \App\Repository\UserRepository $userRepository
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}", name="user_delete", methods={"GET"})
+     *
+     * @param \App\Entity\User $user
+     * @param                  $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function delete(User $user, $id): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        if ($id) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('user_index');
+    }
+}
