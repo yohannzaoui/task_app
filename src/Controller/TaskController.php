d98f147fa7ee06718d94d5d6a895f78c4e9fe0b8
Yohann Zaoui <yohannzaoui@gmail.com>
2019-03-28 15:37:48 +0100

    refactor taskController

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,414 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yohann
+ * Date: 28/03/19
+ * Time: 15:26
+ */
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+
+use App\Form\EditTaskType;
+use App\FormHandler\EditTaskFormHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\FormHandler\CreateTaskFormHandler;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Event\FileRemoverEvent;
+use App\Event\TaskByEmailEvent;
+use App\Event\TaskToMyEmailEvent;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class TaskController
+ *
+ * @package App\Controller
+ */
+class TaskController extends AbstractController
+{
+    /**
+     * @Route(path="/", name="tasks", methods={"GET"})
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $tasks = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->findBy([
+                'done'=> false,
+                'pin' => false,
+                'author' => $this->getUser()
+            ]);
+
+        $tasksPin = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->findBy([
+                'done'=> false,
+                'pin' => true,
+                'author' => $this->getUser()
+            ]);
+
+        return $this->render('task/index.html.twig', [
+            'tasks' => $tasks,
+            'tasksPin' => $tasksPin,
+            'title' => 'Mes tâches'
+
+        ]);
+    }
+
+
+    /**
+     * @Route(path="/task/show/{id}", name="show_task", methods={"GET", "POST"})
+     *
+     * @param $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function show(string $id): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        if (!$task){
+            throw new \Exception('Pas de tâche avec cet ID');
+        }
+
+        return $this->render('task/show.html.twig', [
+            'task' => $task,
+            'title' => $task->getTitle()
+        ]);
+    }
+
+
+    /**
+     * @Route(path="/task/create", name="create_task", methods={"GET", "POST"})
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\FormHandler\CreateTaskFormHandler    $handler
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function create(Request $request, CreateTaskFormHandler $handler): Response
+    {
+        $task = new Task();
+
+        $form = $this->createForm(TaskType::class, $task)
+            ->handleRequest($request);
+
+        if ($handler->handle($form, $task)){
+
+            return $this->redirectToRoute('show_task', ['id' => $task->getId()]);
+        }
+        return $this->render('task/create.html.twig', [
+            'form' =>$form->createView(),
+            'title' => 'Créer une tâche'
+        ]);
+    }
+
+
+    /**
+     * @Route(path="/task/edit/{id}", name="edit_task", methods={"GET", "POST"})
+     *
+     * @param                                                $id
+     * @param \Symfony\Component\HttpFoundation\Request      $request
+     * @param \App\FormHandler\EditTaskFormHandler           $handler
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function edit(string $id, Request $request, EditTaskFormHandler $handler): Response
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        if (!$task){
+            throw new \Exception('Pas de tâche avec cet ID');
+        }
+
+        $form = $this->createForm(EditTaskType::class, $task)
+            ->handleRequest($request);
+
+        $this->denyAccessUnlessGranted('access', $task);
+
+
+        if ($handler->handle($form, $task)){
+
+            return $this->redirectToRoute('show_task', ['id' => $task->getId()]);
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Modifier la tâche'
+        ]);
+    }
+
+
+    /**
+     * @Route(path="/task/delete/{id}", name="delete_task", methods={"GET"})
+     *
+     * @param                                                             $id
+     * @param \Doctrine\Common\Persistence\ObjectManager                  $manager
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function delete(string $id, ObjectManager $manager, EventDispatcherInterface $eventDispatcher): Response
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        if (!$task){
+            throw new \Exception('Pas de tâche avec cet ID ');
+        }
+
+        $this->denyAccessUnlessGranted('access', $task);
+
+        $eventDispatcher->dispatch(
+            FileRemoverEvent::NAME,
+            new FileRemoverEvent($task->getImage()
+            )
+        );
+        $task->setImage(null);
+
+        $manager->remove($task);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Tâche supprimer'
+        );
+
+        return $this->redirectToRoute('tasks');
+    }
+
+
+    /**
+     * @Route(path="/task/delete/image/{id}", name="delete_task_image", methods={"GET"})
+     *
+     * @param                                                             $id
+     * @param \Doctrine\Common\Persistence\ObjectManager                  $manager
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteImage(string $id, ObjectManager $manager, EventDispatcherInterface $eventDispatcher): Response
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        $this->denyAccessUnlessGranted('access', $task);
+
+        $eventDispatcher->dispatch(
+            FileRemoverEvent::NAME,
+            new FileRemoverEvent($task->getImage()
+            )
+        );
+
+        $task->setImage(null);
+
+        $manager->flush();
+
+        return $this->redirectToRoute('show_task', ['id' => $task->getId()]);
+    }
+
+
+    /**
+     * @Route(path="/task/done/{id}", name="done_task", methods={"GET"})
+     *
+     * @param                                            $id
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function done(string $id, ObjectManager $manager): Response
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        if (!$task){
+            throw new \Exception('Pas de tâche avec cet ID');
+        }
+
+        $this->denyAccessUnlessGranted('access', $task);
+
+        if ($task->isDone() == true){
+            $task->notDone();
+
+            $manager->flush();
+
+            return $this->redirectToRoute('tasks');
+        }
+
+        $task->done();
+        $task->setPin(false);
+        $task->doneDate();
+
+        $manager->flush();
+
+        return $this->redirectToRoute('view_done_task');
+    }
+
+
+    /**
+     * @Route(path="/task/done", name="view_done_task", methods={"GET"})
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function doneTask(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $tasks = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->findBy([
+                'done'=> true,
+                'author' => $this->getUser()
+            ]);
+
+        return $this->render('task/done.html.twig', [
+            'tasks' => $tasks,
+            'title' => 'Mes tâches terminées'
+        ]);
+    }
+
+
+    /**
+     * @Route(path="/task/pin/{id}", name="task_pin", methods={"GET"})
+     *
+     * @param                                            $id
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function pin(string $id, ObjectManager $manager): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $taskPin = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        if (!$taskPin){
+            throw new \Exception('no task with this ID');
+        }
+
+        if ($taskPin->getPin() == true){
+            $taskPin->notPin();
+
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'La tâche n\'est plus épinglée'
+            );
+
+            return $this->redirectToRoute('tasks');
+        }
+
+        $taskPin->pin();
+
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+
+            'Tâche épinglée'
+        );
+
+        return $this->redirectToRoute('tasks');
+    }
+
+
+    /**
+     * @Route(path="/send/task/byEmail", name="send_task_byEmail", methods={"GET", "POST"})
+     *
+     * @param \Symfony\Component\HttpFoundation\Request                   $request
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function sendByEmail(Request $request, EventDispatcherInterface $eventDispatcher): Response
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($request->request->get('task_id'));
+
+        if (!$task){
+            throw new \Exception('Pas de tâche avec cet ID');
+        }
+
+        if ($this->isCsrfTokenValid('email', $request->request->get('_csrf_token'))){
+            $this->denyAccessUnlessGranted('send', $task);
+
+            $eventDispatcher->dispatch(
+                TaskByEmailEvent::NAME,
+                new TaskByEmailEvent(
+                    $request->request->get('email'),
+                    $task->getTitle(),
+                    $task->getContent()
+                )
+            );
+
+            $this->addFlash(
+                'success',
+                'Votre tâche à bien été envoyer à : '.$request->request->get('email')
+            );
+            return $this->redirectToRoute('show_task', ['id' => $task->getId()]);
+        }
+    }
+
+
+    /**
+     * @Route(path="/send/task/myEmail/{id}", name="send_task_myEmail", methods={"GET"})
+     *
+     * @param                                                             $id
+     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function sendToMyEmail(string $id, EventDispatcherInterface $eventDispatcher): Response
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        $this->denyAccessUnlessGranted('access', $task);
+
+        $eventDispatcher->dispatch(
+            TaskToMyEmailEvent::NAME,
+            new TaskToMyEmailEvent(
+                $this->getUser()->getEmail(),
+                $task->getTitle(),
+                $task->getContent()
+            ));
+
+        $this->addFlash(
+            'success',
+            'Tâche envoyer sur votre adresse email : '.$this->getUser()->getEmail()
+        );
+
+        return $this->redirectToRoute('tasks');
+    }
+}
